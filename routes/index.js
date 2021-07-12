1bc7812dc5c235493def3d967ffa0b05408d2c98
Piyush Pandey <piyushpvns@gmail.com>
2021-07-12 23:30:30 +0530

    add post route and controller

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,12 @@ const homeController = require('../controllers/home_controller');
 console.log('router loaded');
 
 
-router.get('/', homeController.home);
+router.get('/', homeController.home2);
+//router.get('/home2', homeController.home2);
 router.use('/users', require('./users'));
+
+//for any further routes,access from here
+//router.use('/routerName', require('./routerFile'));
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,5 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.profile);
-
+router.use('/profile/post', require('./post'))
 module.exports = router;  
