27c79c8fd0a07a6b43d3a5a10a4b2ec165dbb0d5
Piyush Pandey <piyushpvns@gmail.com>
2021-07-14 23:58:35 +0530

    Update home_controller.js

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,12 @@
 //export
-module.exports.home = function(req, res, next) {
-    return res.end('<h1>Express is up for Codeial!</h1>')
+module.exports.home = function(req, res) {
+    return res.render('home', {
+        title: "Home"
+    });
 }
 
+/*
 module.exports.home2 = function(req, res){
     return res.end('<h1> I am the Flash,the fastest man alive </h1>');
 }
+*/
