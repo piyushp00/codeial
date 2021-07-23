16c8b4e6c0306b9c3eb5174c111676fb157459d0
Piyush Pandey <piyushpvns@gmail.com>
2021-07-24 00:53:51 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const usersController = require("../controllers/users_controller");
 
 
 //! routes
-router.get("/profile", usersController.profile);
+router.get("/profile", passport.checkAuthentication, usersController.profile);
 
 router.get("/sign-up", usersController.signUp);
 
