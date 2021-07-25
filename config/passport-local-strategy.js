a1eb020665e901239ce27343db1b8e815bb64efe
Piyush Pandey <piyushpvns@gmail.com>
2021-07-25 12:22:52 +0530

    Update passport-local-strategy.js

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -62,12 +62,14 @@ passport.checkAuthentication = function(req, res, next){
 }
 
 
-//! if authenticated
+//! if authenticated(set the user for the views)
 passport.setAuthenticatedUser = function(req, res, next){
   if(req.isAuthenticated()){
     //req.user contains the current signed in user from the session cookie and we are just sending this to the locals for the views
     res.locals.user = req.user;
   }
+
+  next();
 }
 
 module.exports = passport;
