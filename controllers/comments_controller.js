3e05d3054957e42b506582b1abcc2e90a4ac55ae
Piyush Pandey <piyushpvns@gmail.com>
2021-07-28 10:19:46 +0530

    Final update to comments route & action

---
+++ controllers/comments_controller.js
@@ -0,0 +1,28 @@
+const Comment = require("../models/comment");
+const Post = require("../models/post");
+
+module.exports.create = function (req, res) {
+  Post.findById(req.body.post, function (error, post) {
+    
+    if (post) {
+      Comment.create(
+        {
+          content: req.body.content,
+          post: req.body.post,
+          user: req.user._id,
+        },
+        function (error, comment) {
+          if (error) {
+            console.log("error in creating comment");
+            return;
+          }
+
+          post.comments.push(comment);
+          post.save();
+
+          res.redirect("/");
+        }
+      );
+    } 
+  });
+};
