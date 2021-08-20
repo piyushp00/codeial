98b0cb464ac0b77ccd00f975592e19073fb0207f
Piyush Pandey <piyushpvns@gmail.com>
2021-08-21 00:11:02 +0530

    Update home_posts.js

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -11,7 +11,9 @@
         url: "/posts/create",
         data: newPostForm.serialize(),
         success: function (data) {
-          console.log(data);
+          let newPost = newPostDom(data.data.post);
+          $("#posts-list-container>ul").prepend(newPost);
+          deletePost($(' .delete-post-button', newPost));
         },
         error: function (error) {
           console.log(error.responseText);
@@ -23,43 +25,55 @@
   //method to create a post in DOM
   let newPostDom = function (post) {
     return $(`<li id="post-${post._id}">
-    <!--posts-->
-    <p>
-        <% if((locals.user) && (locals.user.id == post.user.id)) { %> 
-            <small>
-                <a  class="delete-post-button" href="/posts/destroy/${post._id} "><i class="far fa-trash-alt"></i></a>
-            </small>
-        <% } %>     
-        ${post.content}
-        <br>
+                <!--posts-->
+                <p>
+                    <small>
+                      <a class="delete-post-button" href="/posts/destroy/${ post._id } "><i class="far fa-trash-alt"></i></a>
+                    </small>
+           
+                  ${ post.content }
+                  <br>
 
-        <small> ${post.user.name} </small>
-    </p>
+                  <small> ${ post.user.name } </small>
+                </p>
 
-    <!--comments form-->
-    <div class="post-comments">
+                <!--comments form-->
+                <div class="post-comments">
 
         
-            <form action="/comments/create" method="POST">
-                <input type="text" name="content" placeholder="Type Here..." required>
-                <input type="hidden" name="post" value="${post._id}">
-                <input type="submit" value="Add Comment">
-            </form>
+                  <form action="/comments/create" method="POST">
+                    <input type="text" name="content" placeholder="Type Here..." required>
+                    <input type="hidden" name="post" value="${post._id}">
+                    <input type="submit" value="Add Comment">
+                  </form>
         
 
-        <div class="post-comments-list">
-            <ul id="post-comments-${post._id}> ">
-                <% for(comment of post.comments) { %>
-                    
-                    <%- include('_comment') -%>
-                    
-                <% } %>   
-            </ul>
-        </div>
+                  <div class="post-comments-list">
+                    <ul id="post-comments-${ post._id }> ">
+                    </ul>
+                  </div>
 
-    </div>
-</li>`);
+                </div>
+              </li>`);
   };
+
+  //method to delete a post from the dom
+  let deletePost = function(deleteLink){
+    $(deleteLink).click(function(e){
+      e.preventDefault();
+
+      $.ajax({
+        type: 'get',
+        url: $(deleteLink).prop('href'),
+        success: function(data){
+          $(`#post-${data.data.post_id}` ).remove();
+        },
+        error: function(error){
+          console.log(error.responseText);
+        }
+      })
+    })
+  }
 
   createPost();
 }
